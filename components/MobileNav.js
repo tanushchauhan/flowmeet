07a527f1588da2878e62c234a85ff849bc22d9e0
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-07-16 13:13:43 -0500

    Completed the Navbar and the sidebar.

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -1,14 +1,18 @@
+"use client";
+
 import {
   Sheet,
+  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { sidebar } from "@/utils/sidebar";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function MobileNav() {
+  const pathname = usePathname();
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
@@ -21,10 +25,56 @@ function MobileNav() {
             className=" cursor-pointer sm:hidden"
           ></Image>
         </SheetTrigger>
-        <SheetContent
-          side="left"
-          className="order-none bg-dark-1"
-        ></SheetContent>
+        <SheetContent side="left" className="order-none bg-dark-1">
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src="/icons/logo_FlowMeet.svg"
+              width={250}
+              height={105}
+              alt="Flow Meet Logo"
+              className="max-sm:hidden"
+              priority
+            ></Image>
+            <Image
+              src="/icons/logo.svg"
+              width={92}
+              height={25}
+              alt="Flow Meet Logo"
+              className="hidden max-sm:block"
+              priority
+            ></Image>
+          </Link>
+          <div className="flex h-[calc(100vh - 72 px)] flex-col justify-between overflow-auto">
+            <SheetClose asChild>
+              <section className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebar.map((e) => {
+                  return (
+                    <SheetClose asChild key={e.route}>
+                      <Link
+                        href={e.route}
+                        key={e.text}
+                        className={`flex gap-4 items-center p-4 rounded-lg w-full max-w-60 ${
+                          pathname === e.route ||
+                          (pathname.startsWith(e.route) && e.route !== "/")
+                            ? "bg-blue-1"
+                            : ""
+                        }`}
+                      >
+                        <Image
+                          src={e.imgUrl}
+                          alt={e.text}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{e.text}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
+        </SheetContent>
       </Sheet>
     </section>
   );
